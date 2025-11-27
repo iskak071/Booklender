d07ed86ed3b2953922ced96df7a5c3d846964932
Iskak <isxak3120@gmail.com>
2025-11-27 17:01:32 +0600

    feat:   - В классе сотрудника добавлены новые поля email и password, и соответственно геттеры и сеттеры

--- src/kg/attractor/java/lesson44/models/Employee.java
+++ src/kg/attractor/java/lesson44/models/Employee.java
@@ -3,6 +3,8 @@ package kg.attractor.java.lesson44.models;
 public class Employee {
     private int id;
     private String fullName;
+    private String email;
+    private String password;
 
     public Employee() {}
 
@@ -12,11 +14,23 @@ public class Employee {
     public String getFullName() {
         return fullName;
     }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
     public void setId(int id) {
         this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
